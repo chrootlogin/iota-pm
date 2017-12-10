3f6e51d874017b473c96388026986293d713bdad
Simon Erhardt <me@rootlogin.ch>
2017-12-10 15:23:21 +0100

    first commit

---
+++ root/docker-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "${IOTA_NODE}" ]; then
+  echo "Please set IOTA_NODE environment variable."
+  exit 1
+fi
+
+chown -R iota-pm:iota-pm /opt/iota-pm
+
+cd /opt/iota-pm
+
+exec su-exec iota-pm iota-pm -i ${IOTA_NODE} -p 0.0.0.0:8888
